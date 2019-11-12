2a87404a4f0c27abf08995e545965d86cb776072
ngochiep30051998 <ngochiep30051998@gmail.com>
2019-11-12 18:01:31 +0700

    swipe

--- www/js/javascript.js
+++ www/js/javascript.js
@@ -3,35 +3,36 @@
 
 
 
-$(document).ready(function() {
-  // Handler for .ready() called.
-  var currentPage = 0;
-  $(".book")
-    .on("click", ".active", nextPage)
-    .on("click", ".flipped", prevPage);
+$(document).ready(function () {
+    // Handler for .ready() called.
+    var currentPage = 0;
+    $(".book")
+        .on("click", ".active", nextPage)
+        .on("click", ".flipped", prevPage);
 
-  $(".book")
-    .hammer()
-    .on("swipeleft", nextPage);
-  $(".book")
-    .hammer()
-    .on("swiperight", prevPage);
+    $(".image")
+        .hammer()
+        .on("swipeleft", nextPage);
+    $(".image")
+        .hammer()
+        .on("swiperight", prevPage);
 
-  function prevPage() {
-    $(".flipped")
-      .last()
-      .removeClass("flipped")
-      .addClass("active")
-      .siblings(".page")
-      .removeClass("active");
-  }
-  function nextPage() {
-    $(".active")
-      .removeClass("active")
-      .addClass("flipped")
-      .next(".page")
-      .addClass("active")
-      .siblings();
-  }
-  
+    function prevPage() {
+        $('.flipped')
+            .last()
+            .removeClass('flipped')
+            .addClass('active')
+            .siblings('.page')
+            .removeClass('active');
+    }
+    function nextPage() {
+        console.log('nextPage')
+        $('.active')
+            .removeClass('active')
+            .addClass('flipped')
+            .next('.page')
+            .addClass('active')
+            .siblings();
+    }
+
 });
