890d1e76996932ad69e6d0c4dbd3292e8ed537e0
Ngat Do <ngat.do@powergatesoftware.com>
2019-11-12 15:57:10 +0700

    Css for effect slider

---
+++ www/js/javascript.js
@@ -0,0 +1,37 @@
+
+
+
+
+
+$(document).ready(function() {
+  // Handler for .ready() called.
+  var currentPage = 0;
+  $(".book")
+    .on("click", ".active", nextPage)
+    .on("click", ".flipped", prevPage);
+
+  $(".book")
+    .hammer()
+    .on("swipeleft", nextPage);
+  $(".book")
+    .hammer()
+    .on("swiperight", prevPage);
+
+  function prevPage() {
+    $(".flipped")
+      .last()
+      .removeClass("flipped")
+      .addClass("active")
+      .siblings(".page")
+      .removeClass("active");
+  }
+  function nextPage() {
+    $(".active")
+      .removeClass("active")
+      .addClass("flipped")
+      .next(".page")
+      .addClass("active")
+      .siblings();
+  }
+  
+});
